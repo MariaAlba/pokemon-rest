b630626567267018e6b86e50c5ba9b8a36ac7aef
Maria <Curso@PORT-Z12.ipartekaula.com>
2020-02-24 12:31:29 +0100

    nuevo script bbdd

---
+++ src/main/resources/pokescript.sql
@@ -0,0 +1,142 @@
+CREATE DATABASE  IF NOT EXISTS `pokemon` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `pokemon`;
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: pokemon
+-- ------------------------------------------------------
+-- Server version	8.0.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `habilidad`
+--
+
+DROP TABLE IF EXISTS `habilidad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `habilidad` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `habilidad`
+--
+
+LOCK TABLES `habilidad` WRITE;
+/*!40000 ALTER TABLE `habilidad` DISABLE KEYS */;
+INSERT INTO `habilidad` VALUES (6,'ascua'),(5,'electricidad estatica'),(2,'foco interno'),(7,'hedor'),(1,'impasible'),(3,'justiciero'),(4,'pararayos');
+/*!40000 ALTER TABLE `habilidad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pokemon`
+--
+
+DROP TABLE IF EXISTS `pokemon`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pokemon` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `imagen` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pokemon`
+--
+
+LOCK TABLES `pokemon` WRITE;
+/*!40000 ALTER TABLE `pokemon` DISABLE KEYS */;
+INSERT INTO `pokemon` VALUES (2,'pikachu','https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png'),(3,'charmander','https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'),(4,'bulbasur','https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'),(98,'miau','https://f0.pngfuel.com/png/295/911/pokemon-pokeball-png-clip-art.png');
+/*!40000 ALTER TABLE `pokemon` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pokemon_has_habilidades`
+--
+
+DROP TABLE IF EXISTS `pokemon_has_habilidades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pokemon_has_habilidades` (
+  `id_pokemon` int(11) NOT NULL,
+  `id_habilidad` int(11) NOT NULL,
+  PRIMARY KEY (`id_pokemon`,`id_habilidad`),
+  KEY `FK_pokemon_idx` (`id_pokemon`),
+  KEY `FK_habilidad_idx` (`id_habilidad`),
+  CONSTRAINT `FK_habilidad` FOREIGN KEY (`id_habilidad`) REFERENCES `habilidad` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `FK_pokemon` FOREIGN KEY (`id_pokemon`) REFERENCES `pokemon` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pokemon_has_habilidades`
+--
+
+LOCK TABLES `pokemon_has_habilidades` WRITE;
+/*!40000 ALTER TABLE `pokemon_has_habilidades` DISABLE KEYS */;
+INSERT INTO `pokemon_has_habilidades` VALUES (2,4),(2,5),(3,1),(3,3),(3,6),(4,1),(4,7),(98,3),(98,6);
+/*!40000 ALTER TABLE `pokemon_has_habilidades` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'admin','admin');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'pokemon'
+--
+
+--
+-- Dumping routines for database 'pokemon'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-02-24 12:29:51
